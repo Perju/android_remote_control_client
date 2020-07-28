728a40efa4ae968f0299ab1cf26ac5082410433b
JuanP Pérez <yohanam.paulus@gmail.com>
2020-07-28 22:47:15 +0200

    Added basic conection form

--- lib/appConfigure.dart
+++ lib/appConfigure.dart
@@ -16,6 +16,69 @@ class AppConfiguration extends StatelessWidget {
           },
         ),
       ),
+      body: Center(
+        child: SizedBox(
+          width: 480,
+          child: ConfigForm(),
+        ),
+      ),
+    );
+  }
+}
+
+class ConfigForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ConfigFormState();
+  }
+}
+
+class ConfigFormState extends State<ConfigForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  static const decoration = InputDecoration(
+    fillColor: Colors.white,
+    filled: true,
+  );
+
+  var servidor = TextFormField(
+    decoration: decoration.copyWith(
+      labelText: "Dirección IP",
+      hintText: "192.xxx.xxx.xxx",
+    ),
+    validator: (value) {
+      if (value.isEmpty) {
+        return 'Campo vacío';
+      }
+      return null;
+    },
+  );
+  var puerto = TextFormField(
+    decoration: decoration.copyWith(
+      labelText: "Puerto",
+      hintText: "4000",
+    ),
+    validator: (value) {
+      if (value.isEmpty) {
+        return 'Campo vacío';
+      }
+      return null;
+    },
+  );
+
+  var conectar = RaisedButton(
+    onPressed: () {},
+    child: Text("Conectar"),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[servidor, puerto, conectar],
+      ),
     );
   }
 }
