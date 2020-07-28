993d5be7af3c61b2705d39d8a1ab111fb029d822
JuanP Pérez <yohanam.paulus@gmail.com>
2020-07-28 22:45:49 +0200

    Updated proyect files for latest flutter versions

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/jupa/develop/flutter"
-export "FLUTTER_APPLICATION_PATH=/media/jupa/Almacen/PROYECTOS_PROGRAMACION/rc_androidx"
+export "FLUTTER_ROOT=/home/jupa/develop/SDKs/flutter"
+export "FLUTTER_APPLICATION_PATH=/media/jupa/Almacen/PROYECTOS_PROGRAMACION/rc_car/RC_Android_remote_control"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/jupa/develop/flutter/bin/cache/artifacts/engine/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/jupa/develop/SDKs/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
